cef318377553a308e580ca8300812197d413a3f2
JSH <hoko7022@naver.com>
2023-10-24 16:58:21 +0900

    부트초기설정

---
+++ src/main/java/com/example/ssurvey/SsurveyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ssurvey/SsurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
